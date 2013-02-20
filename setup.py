7a9dd69a68160ab35b441a06e6e9f5e3d7afc725
Stefan Urbanek <stefan@agentfarms.net>
2013-02-20 17:38:30 +0100

    updated version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cubes_search",
-    version = '0.2.0',
+    version = '0.3.0',
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
